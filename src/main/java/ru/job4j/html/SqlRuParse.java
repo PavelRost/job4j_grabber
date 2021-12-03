e2d08e6c8aa0787957ed310d3e5060bbeb3f93ca
Rostmp <84409690+Rostmp@users.noreply.github.com>
2021-12-03 21:09:23 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,19 +4,49 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
+import ru.job4j.grabber.Post;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
-public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
-        for (int i = 1; i < 6; i++) {
-            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SqlRuParse implements Parse {
+
+    private final SqlRuDateTimeParser sqlRuDateTimeParser;
+
+    public SqlRuParse(SqlRuDateTimeParser sqlRuDateTimeParser) {
+        this.sqlRuDateTimeParser = sqlRuDateTimeParser;
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> rsl = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(link).get();
             Elements row = doc.select(".postslisttopic");
             for (Element td : row) {
-                Element parent = td.parent();
-                Element href = td.child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                System.out.println(parent.child(5).text());
+                rsl.add(detail(td.child(0).attr("href")));
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return rsl;
+    }
+
+    @Override
+    public Post detail(String link) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(link).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String description = doc.select(".msgBody").get(1).text();
+        String title = doc.select(".messageHeader").get(0).ownText();
+        LocalDateTime created = sqlRuDateTimeParser.parse(doc.select(".msgFooter").get(0).text().split(" \\[")[0]);
+        return new Post(title, link, description, created);
     }
 }
