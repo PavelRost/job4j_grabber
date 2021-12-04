6187dcfeda42ac6d06d3a060f720e180a038e9a9
Rostmp <84409690+Rostmp@users.noreply.github.com>
2021-12-04 11:26:55 +0300

    2.4. SqlRuParse [#285213 #235647]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -6,6 +6,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import ru.job4j.grabber.Parse;
 import ru.job4j.grabber.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
@@ -15,23 +16,30 @@ import java.util.List;
 
 public class SqlRuParse implements Parse {
 
-    private final SqlRuDateTimeParser sqlRuDateTimeParser;
+    private final DateTimeParser dateTimeParser;
 
-    public SqlRuParse(SqlRuDateTimeParser sqlRuDateTimeParser) {
-        this.sqlRuDateTimeParser = sqlRuDateTimeParser;
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
     }
 
     @Override
     public List<Post> list(String link) {
         List<Post> rsl = new ArrayList<>();
-        try {
-            Document doc = Jsoup.connect(link).get();
-            Elements row = doc.select(".postslisttopic");
-            for (Element td : row) {
-                rsl.add(detail(td.child(0).attr("href")));
+        int index = link.lastIndexOf('/');
+        String linkNew = link.substring(0, index + 1);
+        for (int i = 1; i < 6; i++) {
+            try {
+                Document doc = Jsoup.connect(linkNew + i).get();
+                Elements row = doc.select(".postslisttopic");
+                for (Element td : row) {
+                    Post temp = detail(td.child(0).attr("href"));
+                    if (temp.getTitle().matches(".*\\bJava\\b.*")) {
+                        rsl.add(temp);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
         return rsl;
     }
@@ -46,7 +54,7 @@ public class SqlRuParse implements Parse {
         }
         String description = doc.select(".msgBody").get(1).text();
         String title = doc.select(".messageHeader").get(0).ownText();
-        LocalDateTime created = sqlRuDateTimeParser.parse(doc.select(".msgFooter").get(0).text().split(" \\[")[0]);
+        LocalDateTime created = dateTimeParser.parse(doc.select(".msgFooter").get(0).text().split(" \\[")[0]);
         return new Post(title, link, description, created);
     }
 }
